2532a3c1f880a52009c6f91f6bc5481901c37cec
Queisme <queiskey@gmail.com>
2024-07-25 07:16:08 -0500

    playin in the sandbox

---
+++ Projects/SandBox/script.js
@@ -0,0 +1,24 @@
+// const repeatString = (str, num) => {
+//   return num < 0 ? 'ERROR' : str.repeat(num);
+// };
+
+// console.log(repeatString('hey', 4));
+
+// const reverseString = (str) => {
+//   return [...str].reverse().join('');
+// };
+
+// console.log(reverseString('Pooh'));
+
+// const removeFromArray = (arrs, ...reMove) => {
+//   const trying = new Set(reMove);
+//   return arrs.filter((value) => !trying.has(value));
+// };
+
+// console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ));
+
+//sumAll
+
+const sumAll = () => {};
+
+console.log(sumAll(1, 4)); //1 + 2 + 3 + 4 //10
