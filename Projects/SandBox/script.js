a311b26a2f4f3b712b0f574d0721f845d682ce95
Queisme <queiskey@gmail.com>
2024-07-28 21:08:47 -0500

    update

--- Projects/SandBox/script.js
+++ Projects/SandBox/script.js
@@ -32,3 +32,11 @@
 // }
 
 // console.log(problem(''));
+
+const leapYears = (year) => {
+  if (year % 4 === 0) return 'true';
+  if (year % 100 === 0 && year % 400 === 0) return 'true';
+  else return 'false';
+};
+
+console.log(leapYears(1900));
